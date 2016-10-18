37f20df0c8d09b10ddff1148afe30268ecc43a66
nicolas <nickh93@outlook.com>
2016-10-18 15:19:44 -0400

    placed all seperate components of application in separate files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,33 +1,26 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <div *ngFor="let currentTask of tasks">
-      <h3>{{ currentTask.description }}</h3>
-      <button (click)="showDetails(currentTask)">Edit</button>
-    </div>
-    <div *ngIf="selectedTask">
-      <h1>Edit Task</h1>
-      <div>
-        <label>Enter a Task Description:</label>
-        <input [(ngModel)] = "selectedTask.description">
-      </div>
-      <div>
-        <label>Enter a Task Id:</label>
-        <input [(ngModel)]="selectedTask.id">
-        <button (click)="finishedEditing()">Done</button>
-      </div>
-    </div>
+    <pies></pies>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+    ></task-list>
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-task>
   </div>
   `
 })
 
 export class AppComponent {
-  favoritePies: string[] = ["Apple", "Banana Cream", "Blackberry"];
-  public tasks: Task[] = [
+  public masterTaskList: Task[] = [
     new Task("Create To-Do list App", 0),
     new Task("Learn Kung Fu", 1),
     new Task("Rewatch all of the Game of Thrones Series", 2),
@@ -41,8 +34,3 @@ export class AppComponent {
     this.selectedTask = null;
   }
  }
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) { }
-}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { AppComponent }   from './app.component';
-
+import { NgModule }          from '@angular/core';
+import { BrowserModule }     from '@angular/platform-browser';
+import { FormsModule }       from '@angular/forms';
+import { AppComponent }      from './app.component';
+import { PiesListComponent } from './pies-list.component';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
 @NgModule ({
   imports: [
     BrowserModule,
-    FormsModule],
-  declarations: [AppComponent],
+    FormsModule
+  ],
+  declarations: [
+    PiesListComponent,
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent
+  ],
   bootstrap:     [AppComponent]
 })
 
