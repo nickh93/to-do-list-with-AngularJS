1115aecd27a227a4eac9b5efa4a603e3e352a418
nicolas <nickh93@outlook.com>
2016-10-18 12:07:53 -0400

    start the to-do lost applications with necessary startung files for and Angular application

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h1>My First 2 App </h1>
-  </div>
   `
 })
 
 export class AppComponent {
 
-}
+ }
+
+ export class Album {
+   
+ }
